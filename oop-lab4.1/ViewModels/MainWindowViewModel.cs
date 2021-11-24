816e588fa0a871db081c1a609a1cafcbf961777b
CyrilSmed <cyrilsmed@outlook.com>
2021-11-24 11:05:16 +0500

    Initialize WPF project template

---
+++ oop-lab4.1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using oop_lab4._1.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lab4._1.ViewModels
+{
+    internal class MainWindowViewModel : ObservableObject
+    {
+        public RelayCommand CommandExample { get; set; }
+
+        public IMainWindowView View { get; set; }
+
+        public MainWindowViewModel()
+        {
+            CommandExample = new RelayCommand(o =>
+           {
+
+           });
+        }
+
+    }
+}
